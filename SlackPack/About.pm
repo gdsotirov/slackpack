d30fd998d9cc33f28adee5672819720f740be94f
Georgi D. Sotirov <gdsotirov@dir.bg>
2006-09-09 16:49:20 +0000

    Add About module which will extract general data about the site - statistics, version information and so on.

---
+++ SlackPack/About.pm
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+#
+# SlackPack
+# Copyright (C) 2006  Georgi D. Sotirov, gsotirov@sotirov-bg.net
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# DESCRIPTION:
+# This is module for extracting of service information and statistics
+#
+# $Id: About.pm,v 1.1 2006/09/09 16:49:20 gsotirov Exp $
+#
+
+package SlackPack::About;
+
+use strict;
+use SlackPack;
+
+sub get_percent_sb {
+  my $dbh = SlackPack->dbh;
+
+  my $query = "SELECT percent_sb()";
+  my @res = $dbh->selectrow_arrayref($query);
+
+  if ( !@res ) {
+    return sprintf("%.2f", 0.0);
+  }
+
+  return sprintf("%.2f", $res[0][0]);
+}
+
+sub get_percent_cur {
+  my $dbh = SlackPack->dbh;
+
+  my $query = "SELECT percent_cur()";
+  my @res = $dbh->selectrow_arrayref($query);
+
+  if ( !@res ) {
+    return sprintf("%.2f", 0.0);
+  }
+
+  return sprintf("%.2f", $res[0][0]);
+}
+
+sub get_dstrbtn_by_arch {
+  my $dbh = SlackPack->dbh;
+
+  my $query = "SELECT * FROM DstrbtnByArch";
+  my $res = $dbh->selectall_arrayref($query, { Slice => {} });
+
+  if ( !$res ) {
+    return {};
+  }
+
+  return $res;
+}
+
+1;
+
