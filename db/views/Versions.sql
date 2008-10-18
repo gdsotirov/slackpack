a4a74eea82288efa3a15ad78e4928173b855404b
Georgi D. Sotirov <gdsotirov@dir.bg>
2008-10-18 20:22:19 +0000

    Add Slackware 12.1, select title instead of name

--- db/views/Versions.sql
+++ db/views/Versions.sql
@@ -1,14 +1,22 @@
 CREATE OR REPLACE VIEW Versions AS
-SELECT p.`name` AS `Name`,
+SELECT p.title AS "Name",
        MAX(p102.version) AS Slack102,
        MAX(p110.version) AS Slack110,
-       MAX(p120.version) AS Slack120
+       MAX(p120.version) AS Slack120,
+       MAX(p121.version) AS Slack121
   FROM packages p
        LEFT JOIN packages p102 ON     p102.name     = p.name
                                   AND p102.slackver = 102
+                                  AND p102.status   = 'ok'
        LEFT JOIN packages p110 ON     p110.name     = p.name
                                   AND p110.slackver = 110
+                                  AND p110.status   = 'ok'
        LEFT JOIN packages p120 ON     p120.name     = p.name
                                   AND p120.slackver = 120
- GROUP BY p.`name`;
+                                  AND p120.status   = 'ok'
+       LEFT JOIN packages p121 ON     p121.name     = p.name
+                                  AND p121.slackver = 121
+                                  AND p121.status   = 'ok'
+ GROUP BY p.name
+ ORDER BY p.title;
 
