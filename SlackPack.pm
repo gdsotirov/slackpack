9591523252f1ae1bff399723a79f46e9ca40e123
Georgi D. Sotirov <gdsotirov@dir.bg>
2014-01-17 20:39:33 +0000

    Change version to 0.4.3 and update copyright

--- SlackPack.pm
+++ SlackPack.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #
 # SlackPack
-# Copyright (C) 2006-2011  Georgi D. Sotirov, gsotirov@sotirov-bg.net
+# Copyright (C) 2006-2014  Georgi D. Sotirov, gsotirov@sotirov-bg.net
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 # DESCRIPTION:
 # The Perl modules used to do all the dirty work
 #
-# $Id: SlackPack.pm,v 1.20 2011/10/11 23:38:25 gsotirov Exp $
+# $Id: SlackPack.pm,v 1.21 2014/01/17 20:39:33 gsotirov Exp $
 #
 
 package SlackPack;
@@ -31,13 +31,13 @@ use SlackPack::Template;
 
 # Change these to your preference
 use constant SP_NAME         => 'SlackPack';
-use constant SP_VERSION      => '0.4.2';
-use constant SP_RELEASE_DATE => '$Date: 2011/10/11 23:38:25 $';
+use constant SP_VERSION      => '0.4.3';
+use constant SP_RELEASE_DATE => '$Date: 2014/01/17 20:39:33 $';
 use constant SP_HOME_URL     => 'http://sotirov-bg.net/slackpack';
 use constant SP_AUTHOR       => 'Georgi D. Sotirov';
 use constant SP_AUTHOR_EMAIL => 'gdsotirov@dir.bg';
 use constant SP_AUTHOR_URL   => 'http://sotirov-bg.net/~gsotirov/';
-use constant SP_COPYRIGHT    => 'Copyright (c) 2005-2011 Georgi D. Sotirov';
+use constant SP_COPYRIGHT    => 'Copyright (c) 2005-2014 Georgi D. Sotirov';
 use constant SP_LOCAL_ROOT   => '/var/ftp/pub/contrib/slackware/packages/';
 
 our $_cache = {};
