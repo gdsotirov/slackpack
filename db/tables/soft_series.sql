e9d7b6ea5ebf2e7541fa7f06c7b6b7d93625a3c8
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-11-18 22:31:57 +0200

    Remove back quotes

--- db/tables/soft_series.sql
+++ db/tables/soft_series.sql
@@ -1,10 +1,10 @@
-CREATE  TABLE `soft_series` (
-  `id`     VARCHAR(8)  NOT NULL,
-  `title`  TEXT        NOT NULL COMMENT 'Description',
+CREATE TABLE soft_series (
+  id    VARCHAR(8)  NOT NULL,
+  title TEXT        NOT NULL COMMENT 'Description',
 
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (id),
 
-  UNIQUE INDEX `idx_softseries_id_unq` (`id` ASC)
+  UNIQUE INDEX idx_softseries_id_unq (id ASC)
 )
 COMMENT = 'Software series';
 
