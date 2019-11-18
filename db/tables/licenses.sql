95431e5af0b73728ced7e205c0d5219d7d7acb52
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-11-18 21:58:12 +0200

    Count only active packages in the database
    Merge not merged changes from SlackPack-0_3-branch (at merge-0_3_2 tag).
    The changes should have been merged on 2007-12-08, but were omitted.

--- db/tables/licenses.sql
+++ db/tables/licenses.sql
@@ -1,11 +1,12 @@
 CREATE TABLE licenses (
-  id          CHAR(8) CHARACTER SET ASCII           NOT NULL  DEFAULT '',
-  `name`      VARCHAR(30) CHARACTER SET ASCII       NOT NULL                COMMENT 'License name',
-  description TEXT                                                          COMMENT 'Short description',
-  url         VARCHAR(256)                                    DEFAULT NULL  COMMENT 'URL with more info about the license or the official page of the license',
-  def         ENUM('no','yes') CHARACTER SET ASCII  NOT NULL  DEFAULT 'no',
-  packages    INT(10) UNSIGNED                      NOT NULL  DEFAULT '0'   COMMENT 'Number of the packages with this license',
-  gpl_compat  ENUM('n','y') CHARACTER SET ASCII               DEFAULT NULL  COMMENT 'Is the license GPL Compatible?',
+  id              CHAR(8)           CHARACTER SET ASCII NOT NULL  DEFAULT '',
+  `name`          VARCHAR(30)       CHARACTER SET ASCII NOT NULL                COMMENT 'License name',
+  description     TEXT                                                          COMMENT 'Short description',
+  url             VARCHAR(256)                                    DEFAULT NULL  COMMENT 'URL with more info about the license or the official page of the license',
+  def             ENUM('no','yes')  CHARACTER SET ASCII NOT NULL  DEFAULT 'no',
+  packages_total  INT(10) UNSIGNED                      NOT NULL  DEFAULT '0'   COMMENT 'Total number of the packages with this license',
+  packages        INT(10) UNSIGNED                      NOT NULL  DEFAULT '0'   COMMENT 'Number of active packages with this license',
+  gpl_compat      ENUM('n','y')     CHARACTER SET ASCII           DEFAULT NULL  COMMENT 'Is the license GPL Compatible?',
 
   PRIMARY KEY  (id),
 
