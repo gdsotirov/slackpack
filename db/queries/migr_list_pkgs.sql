e67dc49f94105757231eae7ec6a0c542d1381bb4
Georgi D. Sotirov <gdsotirov@gmail.com>
2020-03-07 12:51:42 +0200

    Add leptonlib renamed to leptonica and localizations of libbreoffice-langpack and libbreoffice-helppack

--- db/queries/migr_list_pkgs.sql
+++ db/queries/migr_list_pkgs.sql
@@ -1,8 +1,11 @@
 /* List migrated packages grouping renamed and localized */
 SELECT CASE `name`
          WHEN 'icu'                      THEN 'icu4c'
+         WHEN 'leptonlib'                THEN 'leptonica'
          WHEN 'libgksu1.2'               THEN 'libgksu'
          WHEN 'libgksuui1.0'             THEN 'libgksuui'
+         WHEN 'libbreoffice-langpack-bg' THEN 'libbreoffice-langpack'
+         WHEN 'libbreoffice-helppack-bg' THEN 'libbreoffice-helppack'
          WHEN 'mpeg2dec'                 THEN 'libmpeg2'
          WHEN 'openoffice.org-bg'        THEN 'openoffice'
          ELSE `name`
@@ -11,8 +14,11 @@ SELECT CASE `name`
  WHERE versioned = 'g'
  GROUP BY CASE `name`
             WHEN 'icu'                      THEN 'icu4c'
+            WHEN 'leptonlib'                THEN 'leptonica'
             WHEN 'libgksu1.2'               THEN 'libgksu'
             WHEN 'libgksuui1.0'             THEN 'libgksuui'
+            WHEN 'libbreoffice-langpack-bg' THEN 'libbreoffice-langpack'
+            WHEN 'libbreoffice-helppack-bg' THEN 'libbreoffice-helppack'
             WHEN 'mpeg2dec'                 THEN 'libmpeg2'
             WHEN 'openoffice.org-bg'        THEN 'openoffice'
             ELSE `name`
