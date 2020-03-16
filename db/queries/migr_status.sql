609b3d36c6c4e803014578a7c4be69d184a3fcae
Georgi D. Sotirov <gdsotirov@gmail.com>
2020-03-16 18:13:06 +0200

    Replace MySQL specific IFNUL with standard COALESCE

--- db/queries/migr_status.sql
+++ db/queries/migr_status.sql
@@ -1,7 +1,7 @@
 /* Calculate migrated to Git package build scripts */
-SELECT IFNULL(versioned, 'n') versioned, COUNT(*), ROUND(COUNT(*) / pkgs.tot * 100, 3) prcnt
+SELECT COALESCE(versioned, 'n') versioned, COUNT(*), ROUND(COUNT(*) / pkgs.tot * 100, 3) prcnt
   FROM packages,
        (SELECT COUNT(*) tot FROM packages) pkgs
- GROUP BY IFNULL(versioned, 'n')
- ORDER BY IFNULL(versioned, 'n');
+ GROUP BY COALESCE(versioned, 'n')
+ ORDER BY COALESCE(versioned, 'n');
 
