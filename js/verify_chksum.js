f7c056ebb51439f3aaca379c7c11ae18ee4fd2d4
Georgi D. Sotirov <gdsotirov@gmail.com>
2021-06-12 17:51:22 +0300

    Set VerifyAnchor properties directly, add documentation
    Set VerifyAnchor properties directly and set function for onclick
    and onkeypress events to avoid CSP violation for trusted types.
    
    Also add function documentation and set properly window name, so
    just one pop up is shown no matter how many times the user clicks.
    
    See https://w3c.github.io/webappsec-trusted-types/dist/spec/ ,
        https://developer.mozilla.org/en-US/docs/Web/API/Window/open

--- js/verify_chksum.js
+++ js/verify_chksum.js
@@ -1,30 +1,46 @@
-/* Verify checksum functions */
+/**
+ * @file Verify checksum functions
+ * @author Georgi D. Sotirov <gdsotirov@gmail.com>
+ */
 
-function verify_popup(strURL, strTitle) {
+/**
+ * Displays pop up for verification of checksum.
+ * @param {string} strURL URL for the popup window.
+ * @param {string} strWinName Window name.
+ */
+function verify_popup(strURL, strWinName) {
   /* Toolbar and Chrome features */
   var strOptions  = "menubar=no,location=no,resizable=yes,scrollbars=yes,";
   strOptions += "status=no,directories=no,personalbar=no,";
   /* Position and size */
   strOptions += "left=100,top=100,width=400,height=250";
 
-  window.open(strURL, strTitle, strOptions);
+  /* Second argument is name, not title
+   * See https://developer.mozilla.org/en-US/docs/Web/API/Window/open
+   */
+  window.open(strURL, strWinName, strOptions);
 }
 
+/**
+ * Generate checksum verification anchor
+ * @param {number} iPackId Package identifier.
+ */
 function gen_verify_link(iPackId) {
   var VerifyMD5 = document.getElementById('VerifyMD5');
 
   if ( VerifyMD5 != null ) {
     var VerifyAnchor = document.createElement("a");
-    var strAction = "verify_popup('pack.cgi?id=" + iPackId + "&amp;verifymd5=1', ";
-    /* IE doesn't support free text window name as second argument, but
-    * title is anyway generated for the displayed page
-    */
-    strAction += "'_blank'); return false;";
+    var strURL = "pack.cgi?id=" + iPackId + "&amp;verifymd5=1";
 
-    VerifyAnchor.setAttribute("href", "#");
-    VerifyAnchor.setAttribute("onclick", strAction);
-    VerifyAnchor.setAttribute("onkeypress", strAction);
-    VerifyAnchor.appendChild(document.createTextNode(l10n.VERIFY_LNK));
+    var fAction = function() {
+      verify_popup(strURL, 'VerifyChecksum');
+      return false;
+    };
+
+    VerifyAnchor.href       = "#";
+    VerifyAnchor.onclick    = fAction;
+    VerifyAnchor.onkeypress = fAction;
+    VerifyAnchor.text       = l10n.VERIFY_LNK;
     VerifyMD5.appendChild(VerifyAnchor);
   }
   else {
