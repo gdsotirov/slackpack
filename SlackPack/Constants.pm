361754c04ce5eeb5a4601f4f1e97d20a41f7037f
Georgi D. Sotirov <gdsotirov@gmail.com>
2023-01-03 19:48:56 +0200

    Update copyright for 2023

--- SlackPack/Constants.pm
+++ SlackPack/Constants.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #
 # SlackPack
-# Copyright (C) 2006-2021  Georgi D. Sotirov, gdsotirov@gmail.com
+# Copyright (C) 2006-2023  Georgi D. Sotirov, gdsotirov@gmail.com
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -51,7 +51,7 @@ use constant SP_HOME_URL     => 'https://sotirov-bg.net/slackpack';
 use constant SP_AUTHOR       => 'Georgi D. Sotirov';
 use constant SP_AUTHOR_EMAIL => 'gdsotirov@gmail.com';
 use constant SP_AUTHOR_URL   => 'https://sotirov-bg.net/~gsotirov/';
-use constant SP_COPYRIGHT    => 'Copyright (c) 2005-2022 Georgi D. Sotirov';
+use constant SP_COPYRIGHT    => 'Copyright © 2005-2023 Georgi D. Sotirov';
 use constant SP_LOCAL_ROOT   => '/var/ftp/pub/contrib/slackware/packages/';
 use constant SP_LIBPATH      => dirname(dirname(__FILE__));
 use constant SP_CONF         => SP_LIBPATH . "/slackpack.pl.inc";
